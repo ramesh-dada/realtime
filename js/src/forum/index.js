07169d035707cc13a696c31bb8e18d4fb38e8a24
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-09 10:24:02 +0530

    Add files via upload

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,15 +1,14 @@
 import {extend} from 'flarum/common/extend';
 import app from 'flarum/forum/app';
-import DiscussionPage from 'flarum/forum/components/DiscussionPage';
+import PostStream from 'flarum/forum/components/PostStream';
+
 
 /* 1Dot Realtime */
 
 app.initializers.add('ramesh-dada-custom-realtime', () => {
-  extend(DiscussionPage.prototype, 'oncreate', function() {
-    setInterval(()=> {
+  extend(PostStream.prototype, 'view', function (vdom) {
       app.store.find('discussions', this.discussion.id()).then(() => {
         this.stream.update();
       })
-    }, 1000);
 })
 });
