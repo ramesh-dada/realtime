af4d1b213d7029c2e59fdb48d6612a4b6e9172f4
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-09 14:09:58 +0530

    Add files via upload

--- js/src/forum/addDiscussionPagePolling.js
+++ js/src/forum/addDiscussionPagePolling.js
@@ -12,7 +12,7 @@ export default function () {
     });
 
     extend(DiscussionPage.prototype, 'onremove', function () {
-        // Clear timeout when user leaves the discussion
-        clearTimeout(this.pollingInterval);
+        // Clear interval when user leaves the discussion
+        clearInterval(this.pollingInterval);
     });
 }
