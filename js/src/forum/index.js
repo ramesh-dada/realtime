d7c9ef6020ce8bcf7bc568e6f79a039411a5434a
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-09 13:27:41 +0530

    Add files via upload

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,1 +1,8 @@
-a
+import app from 'flarum/forum/app';
+import addDiscussionPagePooling from './addDiscussionPagePolling';
+
+/* 1Dot Realtime */
+
+app.initializers.add('ramesh-dada-custom-realtime', () => {
+    addDiscussionPagePooling();
+});
