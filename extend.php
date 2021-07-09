f6cb8539cceaa8cb7530e2ebb59669a6e1febad4
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-09 10:23:12 +0530

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace ramesh\MobileDiscussionButton;
+namespace ramesh\realtime;
 
 use Flarum\Extend;
 
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__ . '/js/dist/forum.js')
-        ->css(__DIR__ . '/resources/less/forum.less'),
 ];
