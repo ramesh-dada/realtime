2b956efc40b6d6dd770cd6404f2af742e2c75eb5
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-09 09:29:07 +0530

    Add files via upload

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,29 +1,15 @@
 import {extend} from 'flarum/common/extend';
 import app from 'flarum/forum/app';
-import IndexPage from 'flarum/forum/components/IndexPage';
-import Button from 'flarum/common/components/Button';
+import DiscussionPage from 'flarum/forum/components/DiscussionPage';
 
-/* global m */
+/* 1Dot Realtime */
 
-app.initializers.add('ramesh-dada-mobile-flarum-newdiscuss', () => {
-    extend(IndexPage.prototype, 'viewItems', function (items) {
-            const canStartDiscussion = app.forum.attribute('canStartDiscussion') || !app.session.user;
-    items.add(
-      'newDiscussion',
-      Button.component(
-        {
-          icon: 'fas fa-edit',
-          className: 'Button Button--primary IndexPage-newDiscussion',
-          itemClassName: 'DadaDiscuss',
-          onclick: () => {
-            // If the user is not logged in, the promise rejects, and a login modal shows up.
-            // Since that's already handled, we dont need to show an error message in the console.
-            return this.newDiscussionAction().catch(() => {});
-          },
-          disabled: !canStartDiscussion,
-        },
-        app.translator.trans(canStartDiscussion ? 'core.forum.index.start_discussion_button' : 'core.forum.index.cannot_start_discussion_button')
-      )
-    );
-    });
+app.initializers.add('ramesh-dada-custom-realtime', () => {
+  extend(DiscussionPage.prototype, 'oncreate', function() {
+    setInterval(()=> {
+      app.store.find('discussions', this.discussion.id()).then(() => {
+        this.stream.update();
+      })
+    }, 1000);
+})
 });
