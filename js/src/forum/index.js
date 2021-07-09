1ef83ed178d87127175f16e2b211b290de0bfa37
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-09 13:26:19 +0530

    Delete js directory

--- js/src/forum/index.js
+++
@@ -1,8 +0,0 @@
-import app from 'flarum/forum/app';
-import addDiscussionPagePooling from './addDiscussionPagePolling';
-
-/* 1Dot Realtime */
-
-app.initializers.add('ramesh-dada-custom-realtime', () => {
-    addDiscussionPagePooling();
-});
